1f9ff7d0b98a322c28b847cc3661319a743a56a2
tans <trunganhcoder@gmail.com>
2022-04-29 16:56:52 +0700

    upload jobbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Banner from "./components/Banner/Banner";
+import JobBox from "./components/JobBox/JobBox";
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <JobBox />
       <Footer />
     </>
   );
