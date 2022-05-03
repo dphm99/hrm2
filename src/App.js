684e1d79a7ab3196a253642c91eb310d39036310
nguyenduong1803 <duonghd1803@gmail.com>
2022-05-03 17:26:56 +0700

    upload Vancancies

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Banner from "./components/Banner/Banner";
 import JobBox from "./components/JobBox/JobBox";
-import Sliders from "./components/Slider/Slider";
-
+import Vancancies from "./components/Vacancies/Vacancies"
 function App() {
   return (
     <>
       <Header />
-      <Sliders />
-      {/* <Banner /> */}
+      <Banner />
       <JobBox />
+      <Vancancies/>
       <Footer />
     </>
   );
--- src/components/JobBox/JobBox.jsx
+++ src/components/JobBox/JobBox.jsx
@@ -61,6 +61,45 @@ const jobCategory = [
     quantity: "100 việc làm",
   },
 ];
+const jobLists = [
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  },
+  {
+    title: "Nhân viên kinh doanh",
+    quantity: 57
+  }
+]
+
 
 function JobBox() {
   return (
@@ -81,8 +120,31 @@ function JobBox() {
           ))}
         </ul>
       </div>
+      <div className={`d-flex ${styles.jobList}`}> 
+      
+        {jobLists.map((job, index) => (
+         
+            <JobList
+              title={job.title}
+              quantity={job.quantity}
+            />
+            ))}
+            
+      </div>
     </div>
   );
+}
+function JobList(props) {
+  return (
+    <div
+      key={props.index}
+      className={`${styles.JobList}`}
+    >
+      <div className={styles.jobListItem}>
+        <h5>{props.title} ({props.quantity} vị trí) ...</h5>
+      </div>
+    </div>
+  )
 }
 
 export default JobBox;
