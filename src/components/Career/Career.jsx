d6b1447cfba3b368284c8eacbba35e772e35966a
thang17 <86705466+thang17@users.noreply.github.com>
2022-05-03 17:15:07 +0700

    up load

--- src/components/Career/Career.jsx
+++ src/components/Career/Career.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./Career.module.css";
-// import {Link} from "re"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 function Career() {
   const cardList = [
     {
@@ -19,7 +22,51 @@ function Career() {
       text: "Một số cách viết CV cho sinh viên mới ra trường",
       img: "https://img.nhandan.com.vn/Files/Images/2020/07/26/nhat_cay-1595747664059.jpg",
     },
+    {
+      text: "Một số cách viết CV cho sinh viên mới ra trường",
+      img: "https://img.nhandan.com.vn/Files/Images/2020/07/26/nhat_cay-1595747664059.jpg",
+    },
+    {
+      text: "Một số cách viết CV cho sinh viên mới ra trường",
+      img: "https://img.nhandan.com.vn/Files/Images/2020/07/26/nhat_cay-1595747664059.jpg",
+    },
   ];
+  const settings = {
+    //   autoplay:true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+
+    responsive: [
+        
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  };
 
   const CardItem = (props) => {
     return (
@@ -35,13 +82,13 @@ function Career() {
 
           <div className>
             <div className={styles.Career__titlename}>
-            <h5 className={styles.Career__title}>Cách để viết CV</h5>
+              <h5 className={styles.Career__title}>Cách để viết CV</h5>
             </div>
             <div className={styles.Career__tikitiki}>
-            <p className={styles.Career__tiki} maxlength="2">
-              {props.text}
-              {/* <h8 className={styles.Career__h8}>Đọc thêm</h8> */}
-            </p>
+              <p className={styles.Career__tiki}>
+                {props.text}
+                {/* <h8 className={styles.Career__h8}>Đọc thêm</h8> */}
+              </p>
             </div>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group"></div>
@@ -58,14 +105,17 @@ function Career() {
           <h4>ĐỊNH HƯỚNG NGHỀ NGHỆP</h4>
         </div>
         <div class="row">
-          {cardList.map((e, index) => {
-            return <CardItem src={e.img} text={e.text} key={index} />;
-          })}
+          <Slider {...settings}>
+            {cardList.map((e, index) => {
+              return <CardItem src={e.img} text={e.text} key={index} />;
+            })}
+          </Slider>
         </div>
-      </div>
-      <div className={styles.Career__banner}>
+        <div className={styles.Career__banner}>
         <img src="https://dulichvietnam.com.vn/du-lich-anh/wp-content/uploads/2020/04/ngon-ngu-anh-1.jpg" />
       </div>
+      </div>
+      
     </div>
   );
 }
