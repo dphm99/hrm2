3b8445771e33fff8e00687ed39914d705cacb039
tans <trunganhcoder@gmail.com>
2022-05-04 08:58:40 +0700

    upload extension + init Recruit

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loading.css'
+function Loading() {
+  return (
+    <div className="vw-100 vh-100 d-flex justify-content-center align-items-center">
+    <div className="sk-chase">
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+      </div></div>
+  )
+}
+
+export default Loading
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { RecruitProvider } from "./components/contexts/ContextRecuit";
+import Loading from "./components/Loading/Loading";
+
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// const Recruit = lazy(() => import("./pages/Recruit"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <RecruitProvider>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={App} />
+          {/* <Route exact path="/tuyen-dung" component={Recruit} /> */}
+          {/* <Route path="/tuyen-dung/:name" component={Recruit} /> */}
+          <Route path="/*" component={PageNotFound} />
+        </Switch>
+      </Suspense>
+    </RecruitProvider>
+  </BrowserRouter>,
+  root
 );
