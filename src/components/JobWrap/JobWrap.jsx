20005a17b73f8aa71c888319043b6b5a99948fba
thanghn <62323283+thanghn14@users.noreply.github.com>
2022-05-04 09:41:03 +0700

    Update Header-Footer/Create Banner

---
+++ src/components/JobWrap/JobWrap.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../JobWrap/JobWrap.module.css'
+
+function JobWrap() {
+  return (
+    <div>
+        <div className={styles.title}>
+            VỊ TRÍ TUYỂN DỤNG NỔI BẬT
+        </div>
+
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default JobWrap
