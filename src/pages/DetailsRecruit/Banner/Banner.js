b2aed869abd89644ec034ccbd3d7b3d00ce06dd2
thang17 <86705466+thang17@users.noreply.github.com>
2022-05-04 17:16:55 +0700

    banner trang tuyểndung

--- src/pages/DetailsRecruit/Banner/Banner.js
+++ src/pages/DetailsRecruit/Banner/Banner.js
@@ -4,7 +4,7 @@ import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { FacebookShareButton, LinkedinShareButton } from "react-share";
 import { FacebookIcon, LinkedinIcon } from "react-share";
 import zalo from "../../../assets/img/icon-svg/zalo-logo.jpg";
-import demo from "../../../assets/img/icon-svg/1045.jpg"
+import demo from "../../../assets/img/icon-svg/1045.jpg";
 
 const Banner = () => {
   return (
@@ -77,7 +77,7 @@ const Banner = () => {
                       </FacebookShareButton>
                     </div>
                     <div className={styles.Banner_fb}>
-                        <img src={zalo} style={{width:"30px"}}/>
+                      <img src={zalo} style={{ width: "30px" }} />
                     </div>
                     <div className={styles.Banner_fb}>
                       <LinkedinShareButton url="https://www.google.com.vn/search?tbm=isch&q=%E1%BA%A3nh+%C4%91%E1%BA%B9p#imgrc=GvS0Qa0LySjLlM">
@@ -88,12 +88,187 @@ const Banner = () => {
                 </div>
               </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-7">
               <div class="row g-0 border overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <img src={demo}  style={{width:"100%"}}/>
+                <img src={demo} style={{ width: "100%" }} />
                 {/* <div class="col p-4 d-flex flex-column position-static">
                   
                 </div> */}
+                <div>
+                  <h4 className={styles.Banner_hh6}>Mô tả công việc</h4>
+                  <div>
+                    <ul className={styles.Banner_uul}>
+                      <p>
+                        - Tư vấn hỗ trợ khách hàng đến siêu thị Bách Hóa Xanh để
+                        mua sắn, trải nghiệm
+                      </p>
+                      <p>
+                        - Trưng bày, sắp xếp hàng hóa đẩm bảo quầy kệ gọ gàng,
+                        sạch sẽ.
+                      </p>
+                      <p>- Kiểm kê hàng hóa siêu thị.</p>
+                      <p>
+                        - Nhập hàng từ nhà cung cấp hoặc kho tổng (Cân, Nhập,
+                        Kiểm đếm)
+                      </p>
+                      <p>- Vệ sinh siêu thị (Lau dọn sàn,kính,quầy kệ)</p>
+                      <p>- Bừng bê/sắp xếp hàng, quản lý kho hàng gọn gàng</p>
+                      <p>
+                        - Là người nhân viên đa nhiệm, linh hoạt bạn có thể làm
+                        thêm cách vị trí công việc khác như Thu Ngân/Fresh, hoặc
+                        các công việc theo sự phân công của quản lý.
+                      </p>
+                    </ul>
+                  </div>
+                </div>
+
+                <div>
+                  <h4 className={styles.Banner_hh4}>Yêu cầu công việc</h4>
+                  <div>
+                    <ul className={styles.Banner_uul}>
+                      <p>- Nam/nữ từ 18-40 tuổi</p>
+                      <p>- Thân thiện, nhiệt tình, muốn gắn bó lâu dài</p>
+                      <p>- Có sức khỏe tốt, tháo vác, có trách nhiệm.</p>
+                      <p>
+                        - Có điện thoại smartphone để làm việc, chủ động phương
+                        tiện di chuyển (xe máy)
+                      </p>
+                      <p>
+                        - Sẵn sàng làm đa nhiệm: Đi bán hàng ngoài thị trường -
+                        Hỗ trợ vận chuyển - Làm báo cáo
+                      </p>
+                      <p>- Làm fulltime 8tiếng/ ngày (theo giờ hành chính)</p>
+                    </ul>
+                  </div>
+                </div>
+
+                <div>
+                  <h4 className={styles.Banner_hh4}>Chính sách & Phúc lợi</h4>
+                  <div>
+                    <ul className={styles.Banner_uul}>
+                      <p>
+                        - Thu nhập trung bình từ 7-10 triệu trở lên theo hiệu
+                        quả làm việc và giờ công làm việc
+                      </p>
+                      <p>
+                        - Thưởng trong tháng và cuối năm hấp dẫn (Thưởng tết
+                        tương đương 3 tháng thu nhập, số liệu 3 năm gần nhất
+                      </p>
+                      <p>
+                        - Có cơ hội thăng tiến lên Quản lý rõ ràng-làm chủ siêu
+                        thị.
+                      </p>
+                      <p>- Công việc bố trí làm việc thuận tiền gần nhà</p>
+                      <p>
+                        - Có sự hướng dẫn, đạo tạo phát triển kỹ năng/chuyên môn
+                        liên tục
+                      </p>
+                      <p>
+                        - Ký hợp đồng lao động, công việc chính Thức lâu dài -
+                        ỔN định
+                      </p>
+                      <p>- Môi trường làm việc thân thiện, hỗ trợ đồng đội</p>
+                      <p>
+                        - Phúc lợi đầy đủ : Tham gia BHYT , BHXH, Đi du lịch
+                        Teambuilding, 12 ngày phép/ năm, nghĩ thai sản, ốm đau
+                        ...
+                      </p>
+                    </ul>
+                  </div>
+                </div>
+
+                <div>
+                  <h4 className={styles.Banner_hh4}>
+                    Địa điểm làm việc (<a style={{ color: "red" }}>64</a> tỉnh
+                    thành)
+                  </h4>
+                  <div>
+                    <select className={styles.Banner_select}>
+                      <option selected>Toàn quốc</option>
+                    </select>
+                    
+                    <div className={`${styles.Banner_check} form-check`}>
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        defaultValue
+                        id="flexCheckDefault"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexCheckDefault"
+                        style={{ color: "rgb(185, 9, 9)" }}
+                      >
+                        Xem địa điểm làm việc gần bạn nhất
+                      </label>
+                    </div>
+                    <div className={styles.Banner_checkbox}>
+                    <div className={`${styles.Banner_check} form-check`}>
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="flexRadioDefault"
+                        id="flexRadioDefault1"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexRadioDefault1"
+                      >
+                        Tầng 2,Tòa nhà N03T5, khu Đoàn ngoại giao,p.Xuân Tảo,q.Bắc Từ Liêm,Hà Nội, VN.<br/>
+                        <h6 style={{ color: "rgb(185, 9, 9)",fontStyle: "italic" }}>Bản đồ.</h6>
+                      </label>
+                    </div>
+                    <div className={`${styles.Banner_check} form-check`}>
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="flexRadioDefault"
+                        id="flexRadioDefault1"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexRadioDefault1"
+                      >
+                        Số 25, Nguyễn Lương Bằng, phường Tân Phú, Quận 7, TP.Hồ Chí Minh, VN.<br/>
+                        <h6 style={{ color: "rgb(185, 9, 9)",fontStyle: "italic" }}>Bản đồ.</h6>
+                      </label>
+                    </div>
+                    <div className={`${styles.Banner_check} form-check`}>
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="flexRadioDefault"
+                        id="flexRadioDefault1"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexRadioDefault1"
+                      >
+                        Tòa nhà AN06, Nguyễn Chí Thanh,p.Chùa Láng,q.Đống Đa,Hà Nội,VN.<br/>
+                        <h6 style={{ color: "rgb(185, 9, 9)",fontStyle: "italic" }}>Bản đồ.</h6>
+                      </label>
+                    </div>
+                    <div className={`${styles.Banner_check} form-check`}>
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="flexRadioDefault"
+                        id="flexRadioDefault1"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexRadioDefault1"
+                      >
+                          Số 1001, Bàng Đỏ,p.Phường Hoa Phượng Đỏ,tx.Kiến An,Hải Phòng, VN.<br/>
+                        <h6 style={{ color: "rgb(185, 9, 9)",fontStyle: "italic" }}>Bản đồ.</h6>
+                      </label>
+                    </div>
+                    </div>
+                    <div className={styles.Banner__btoon}>
+                      <button className={styles.Banner__btn}>Xem thêm</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
