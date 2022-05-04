6f4e5b8424c2b0ee79f4c262c31b96d921103ea5
thang17 <86705466+thang17@users.noreply.github.com>
2022-05-04 11:04:08 +0700

    CustomBanner

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Loading from "./components/Loading/Loading";
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DetailsRecuit from './pages/DetailsRecruit/DetailsRecuit';
 
 // const Recruit = lazy(() => import("./pages/Recruit"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
@@ -19,6 +20,7 @@ root.render(
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/" component={App} />
+          <Route exact path="/tuyen-dung" component={DetailsRecuit} />
           {/* <Route exact path="/tuyen-dung" component={Recruit} /> */}
           {/* <Route path="/tuyen-dung/:name" component={Recruit} /> */}
           <Route path="/*" component={PageNotFound} />
