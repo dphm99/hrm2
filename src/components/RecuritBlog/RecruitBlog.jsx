2533a09dffbd7e0f3bbf9d5b5bb08c1ac9a23642
Pham Ngoc Duong <dphm99@yahoo.com>
2022-05-07 11:49:13 +0700

    responsive blogdetail + map(recruitBlog)

--- src/components/RecuritBlog/RecruitBlog.jsx
+++ src/components/RecuritBlog/RecruitBlog.jsx
@@ -1,84 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../RecuritBlog/RecruitBlog.module.css";
-
+import { RecruitContext } from "../contexts/ContextRecuit";
 const RecruitBlog = () => {
+  const { data } = useContext(RecruitContext);
+  console.log(data);
   return (
     <>
-      <div className="card mb-4">
-        <div className="card-header py-3">
-          <h5 className="my-0 fw-normal">TIN TUYỂN DỤNG NỔI BẬT</h5>
+      <div
+        className={`${styles.main}card mb-4`}
+        style={{
+          position: `sticky`,
+          top: `11rem`,
+          border: ` 1px solid #ddd`
+
+        }}
+      >
+        <div
+          className="card-header py-3"
+          style={{ borderBottom: `solid 1px #ddd` }}
+
+        >
+          <h5
+            className="my-0 fw-normal"
+          >
+            TIN TUYỂN DỤNG NỔI BẬT</h5>
         </div>
-        <div className="card-header py-3">
-          <div className={`${styles.Block__Normal} col-12`}>
-            <p className={`${styles.Block__Normal_FW} my-0 fw-normal`}>
-              Chuyển viên hỗ trợ kinh doanh
-            </p>
-            <p className={styles.Block__Normal_PP}>04/05/2022</p>
-          </div>
-          <div className={`${styles.Block__Normal55} col-12`}>
-            <p className={styles.Block__Normal_PY}>
-              Hà Nội, TP.Hồ Chí Minh và 2 tỉnh thành khác
-            </p>
-            <h7>ỨNG TUYỂN</h7>
-          </div>
-        </div>
-        <div className="card-header py-3">
-          <div className={`${styles.Block__Normal} col-12`}>
-            <p className={`${styles.Block__Normal_FW} my-0 fw-normal`}>
-              Chuyển viên hỗ trợ kinh doanh
-            </p>
-            <p className={styles.Block__Normal_PP}>04/05/2022</p>
-          </div>
-          <div className={`${styles.Block__Normal55} col-12`}>
-            <p className={styles.Block__Normal_PY}>
-              Hà Nội, TP.Hồ Chí Minh và 2 tỉnh thành khác
-            </p>
-            <h7>ỨNG TUYỂN</h7>
-          </div>
-        </div>
-        <div className="card-header py-3">
-          <div className={`${styles.Block__Normal} col-12`}>
-            <p className={`${styles.Block__Normal_FW} my-0 fw-normal`}>
-              Chuyển viên hỗ trợ kinh doanh
-            </p>
-            <p className={styles.Block__Normal_PP}>04/05/2022</p>
-          </div>
-          <div className={`${styles.Block__Normal55} col-12`}>
-            <p className={styles.Block__Normal_PY}>
-              Hà Nội, TP.Hồ Chí Minh và 2 tỉnh thành khác
-            </p>
-            <h7>ỨNG TUYỂN</h7>
-          </div>
-        </div>
-        <div className="card-header py-3">
-          <div className={`${styles.Block__Normal} col-12`}>
-            <p className={`${styles.Block__Normal_FW} my-0 fw-normal`}>
-              Chuyển viên hỗ trợ kinh doanh
-            </p>
-            <p className={styles.Block__Normal_PP}>04/05/2022</p>
-          </div>
-          <div className={`${styles.Block__Normal55} col-12`}>
-            <p className={styles.Block__Normal_PY}>
-              Hà Nội, TP.Hồ Chí Minh và 2 tỉnh thành khác
-            </p>
-            <h7>ỨNG TUYỂN</h7>
-          </div>
-        </div>
-        <div className="card-header py-3">
-          <div className={`${styles.Block__Normal} col-12`}>
-            <p className={`${styles.Block__Normal_FW} my-0 fw-normal`}>
-              Chuyển viên hỗ trợ kinh doanh
-            </p>
-            <p className={styles.Block__Normal_PP}>04/05/2022</p>
-          </div>
-          <div className={`${styles.Block__Normal55} col-12`}>
-            <p className={styles.Block__Normal_PY}>
-              Hà Nội, TP.Hồ Chí Minh và 2 tỉnh thành khác
-            </p>
-            <h7>ỨNG TUYỂN</h7>
-          </div>
-        </div>
-      </div>
+        {data.map((e, index) => {
+          if (index < 5) {
+            return (
+              <div className="card-header py-3 ">
+                <div className={`row d-flex`}>
+                  <p className={`${styles.jobName} my-0 fw-normal col-8`}>
+                    {e.industry ? e.industry : "Chuyên viên Marketing"}
+                  </p>
+                  <p className={`${styles.deadLine}  col-4`}>
+                    {e.deadline}
+                  </p>
+
+                </div>
+
+                <div className={`row d-flex`}>
+                  <p className={`${styles.address}  col-8`}>
+                    {e.address.name}
+                  </p>
+                  <h7 className={`  col-4`}>
+                    ỨNG TUYỂN
+                  </h7>
+                </div>
+              </div >
+            )
+          }
+        })}
+
+      </div >
     </>
   );
 };
--- src/pages/BlogDetail/BlogDetail.js
+++ src/pages/BlogDetail/BlogDetail.js
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import styles from "./BlogDetail.module.css"
-import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Breadcrumbs from '../../components/BreadCrumb/Breadcrumb'
 import { BlogContext } from '../../components/contexts/ContextBlog'
 import Header2 from '../../components/Header/Header2'
+import RecruitBlog from "../../components/RecuritBlog/RecruitBlog"
+
 function BlogDetail() {
-  const blogIndex = window.location.hash.split("#")[1];
+  // const blogIndex = window.location.hash.split("#")[1];
   const { data } = useContext(BlogContext);
-  
+
   const breadcrumItem = [
     {
       href: "/",
@@ -43,21 +44,23 @@ function BlogDetail() {
           <>
             <div className={`row`}>
               <h1
-                className={`${styles.title} col-xl-8`}
+                className={`${styles.title} col-xl-8 col-lg-8`}
                 style={{ margin: `5rem 0` }}
 
               >
-                {data[0].title}</h1>
+                {data[0].title}
+              </h1>
             </div>
             <div className={`row`}>
               <div
                 dangerouslySetInnerHTML={{ __html: data[0].content }}
-                className={`${styles.content} col-xl-8`}
+                className={`${styles.content} col-xl-8 col-lg-8`}
               >
               </div>
               <div
-                className={`${styles.recruit} col-xl-4`}
+                className={`${styles.recruit} col-xl-4 col-lg-4`}
               >
+                <RecruitBlog className = {`${styles.RecruitBlog}`} />
               </div>
             </div>
           </>
