588e89c5b8d3d6d2720030eb8b569eb372530ffa
thang17 <86705466+thang17@users.noreply.github.com>
2022-05-09 14:57:54 +0700

    resolve

--- src/pages/Blog/Content/BlogContent.jsx
+++ src/pages/Blog/Content/BlogContent.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link , useParams} from "react-router-dom";
 import styles from "./BlogContent.module.css";
 import { BlogContext } from "../../../components/contexts/ContextBlog";
 import RecruitBlog from "../../../components/RecuritBlog/RecruitBlog";
