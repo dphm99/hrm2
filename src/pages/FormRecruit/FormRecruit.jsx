4f6f74d965476498bc30e4b7de4a529200a793aa
Luki Nguyen <nguyenhien236259@gmail.com>
2022-05-13 16:00:15 +0700

    input

--- src/pages/FormRecruit/FormRecruit.jsx
+++ src/pages/FormRecruit/FormRecruit.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext,useRef } from "react";
 import styles from "./FormRecruit.module.css";
 import Header2 from "../../components/Header/Header2";
 import Footer from "../../components/Footer/Footer";
 import Breadcrumbs from "../../components/BreadCrumb/Breadcrumb";
 import axios from "axios";
 import FormData from "form-data";
-import { RecruitContext } from "../../components/contexts/ContextRecuit";
-
+import { RecruitContext } from "../../components/contexts/ContextRecuit"; 
 function FormRecruit() {
   const { data } = useContext(RecruitContext);
 
@@ -17,6 +16,20 @@ function FormRecruit() {
   const [phone, setPhone] = useState();
   const [email, setEmail] = useState();
   console.log(data);
+
+
+  const inputFileRef = useRef( null );
+
+  const onFileChange = ( e ) => {
+    /*Selected files data can be collected here.*/
+    console.log( e.target.files );
+  }
+  const onBtnClick = () => {
+    /*Collecting node-element and performing click*/
+    inputFileRef.current.click();
+  }
+
+
 
   const handleLogin = () => {
     const bodyFormData = new FormData();
@@ -115,13 +128,27 @@ function FormRecruit() {
                 </div>
               </div>
               <div className={styles.inputFile}>
-                <input
+                {/* <input
                   type="file"
                   id="file"
                   className={styles.inputFile}
                   name="file"
                   required
-                />
+                /> */}
+                <input
+        type="file"
+        id="file"
+        ref={inputFileRef}
+        onChange={onFileChange}
+        name="file"
+        style={{display:"none"}}
+      /> 
+                <button 
+                onClick={onBtnClick}
+                className={`${styles.buttonSubmit} ${styles.active}`}
+              >
+                Tải lên CV của bạn
+              </button>
               </div>
               <div className={styles.inputUrl}>
                 <label htmlFor="inputUrl" className={styles.inputLabel}>
