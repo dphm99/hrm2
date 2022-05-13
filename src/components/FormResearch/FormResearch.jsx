9f673e76ededf895a63be49de279b5dcaa3a3bc0
--unset <duonghd1803@gmail.com>
2022-05-13 17:11:05 +0700

    update Research

--- src/components/FormResearch/FormResearch.jsx
+++ src/components/FormResearch/FormResearch.jsx
@@ -2,44 +2,46 @@ import React from 'react'
 import styles from "./FormResearch.module.css"
 function FormResearch() {
     return (
-       <div className="container">
-           <div style={{ textAlign: "center" }} className={styles.Recruit}>
- <div className ={styles.formRecruit}>
-            <form encType="multipart/form-data" style={{marginTop:"18rem"}}>
-                <div className={styles.inputName}>
-                    <input
-                        type="text"
-                        id="inputName"
-                        name="fullname"
-                        className={styles.inputField}
-                        // onChange={(e) => setName(e.target.value)}
-                        placeholder="Số CCCD"
-                        required
-                    />
-                </div>
-                <div className={styles.inputName}>
+        <div className="container">
+            <div style={{ textAlign: "center" }} className={styles.Recruit}>
+                <div className={styles.formRecruit}>
+                    <form encType="multipart/form-data" style={{ marginTop: "18rem" }}>
+                    <h2 className={styles.title}>Tra cứu kết quả ứng tuyển</h2>
+                    <p className={styles.text}>Anh / Chị vui lòng nhập cả 2 thông tin sau đây</p>
+                        <div className={styles.inputName}>
+                            <input
+                                type="text"
+                                id="inputName"
+                                name="fullname"
+                                className={styles.inputField}
+                                // onChange={(e) => setName(e.target.value)}
+                                placeholder="Số CCCD"
+                                required
+                            />
+                        </div>
+                        <div className={styles.inputName}>
 
-                    <input
-                        type="text"
-                        id="inputName"
-                        name="fullname"
-                        className={styles.inputField}
-                        // onChange={(e) => setName(e.target.value)}
-                        placeholder="Số điện thoại"
-                        required
-                    />
+                            <input
+                                type="text"
+                                id="inputName"
+                                name="fullname"
+                                className={styles.inputField}
+                                // onChange={(e) => setName(e.target.value)}
+                                placeholder="Số điện thoại"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            // onClick={handleLogin}
+                            className={`${styles.buttonSubmit} ${styles.active}`}
+                        >
+                            Tra cứu
+                        </button>
+                    </form>
                 </div>
-                <button
-                    type="submit"
-                    // onClick={handleLogin}
-                    className={`${styles.buttonSubmit} ${styles.active}`}
-                >
-                    Tra cứu
-                </button>
-            </form>
+            </div>
         </div>
-        </div>
-       </div>
     )
 }
 
