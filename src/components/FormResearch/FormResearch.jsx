ec9cd2776209bacc64952c8cd1adc491829d8ff1
--unset <duonghd1803@gmail.com>
2022-05-13 17:08:53 +0700

    create Research page

---
+++ src/components/FormResearch/FormResearch.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from "./FormResearch.module.css"
+function FormResearch() {
+    return (
+       <div className="container">
+           <div style={{ textAlign: "center" }} className={styles.Recruit}>
+ <div className ={styles.formRecruit}>
+            <form encType="multipart/form-data" style={{marginTop:"18rem"}}>
+                <div className={styles.inputName}>
+                    <input
+                        type="text"
+                        id="inputName"
+                        name="fullname"
+                        className={styles.inputField}
+                        // onChange={(e) => setName(e.target.value)}
+                        placeholder="Số CCCD"
+                        required
+                    />
+                </div>
+                <div className={styles.inputName}>
+
+                    <input
+                        type="text"
+                        id="inputName"
+                        name="fullname"
+                        className={styles.inputField}
+                        // onChange={(e) => setName(e.target.value)}
+                        placeholder="Số điện thoại"
+                        required
+                    />
+                </div>
+                <button
+                    type="submit"
+                    // onClick={handleLogin}
+                    className={`${styles.buttonSubmit} ${styles.active}`}
+                >
+                    Tra cứu
+                </button>
+            </form>
+        </div>
+        </div>
+       </div>
+    )
+}
+
+export default FormResearch
---
+++ src/pages/Research/Research.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header2 from "../../components/Header/Header2";
+import Footer from "../../components/Footer/Footer";
+import Breadcrumbs from "../../components/BreadCrumb/Breadcrumb";
+import FormResearch from "../../components/FormResearch/FormResearch"
+function Research() {
+    return (
+        <>
+            <Header2 />
+            <FormResearch />
+            <Footer />
+
+        </>
+    )
+}
+
+export default Research
