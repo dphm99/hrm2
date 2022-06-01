87cde8d4ad40252e6d40bf075132ab676462baff
thanghn <62323283+thanghn14@users.noreply.github.com>
2022-06-01 15:56:04 +0700

    Validate form tra-cuu

--- src/components/FormResearch/FormResearch.jsx
+++ src/components/FormResearch/FormResearch.jsx
@@ -1,63 +1,86 @@
-import React from 'react'
-import styles from "./FormResearch.module.css"
-import Infomation from "./Infomation"
+import React from "react";
+import styles from "./FormResearch.module.css";
+import Infomation from "./Infomation";
+
 function FormResearch() {
-    const [info]=React.useState(false)
-    return (
-        <div className={` ${styles.containALl}`}>
-            <div className="container">
-                <div className={`row ${styles.wrap} justify-content-center`} style={{ marginTop: "10rem" }}>
-                    <div className="col-lg-6">
-                        <div style={{ textAlign: "center" }} className={styles.Recruit}>
-                            <div className={styles.formRecruit}>
-                                <form encType="multipart/form-data" >
-                                    <h2 className={styles.title}>Tra cứu kết quả ứng tuyển</h2>
-                                    <p className={styles.text}>Anh / Chị vui lòng nhập cả 2 thông tin sau đây</p>
-                                    <div className={styles.inputName}>
-                                        <input
-                                            type="text"
-                                            id="inputName"
-                                            name="fullname"
-                                            className={styles.inputField}
-                                            // onChange={(e) => setName(e.target.value)}
-                                            placeholder="Số CCCD"
-                                            required
-                                        />
-                                    </div>
-                                    <div className={styles.inputName}>
+  const [info] = React.useState(false);
+  const [phone, setPhone] = React.useState("");
+  const [mail, setMail] = React.useState("");
 
-                                        <input
-                                            type="text"
-                                            id="inputName"
-                                            name="fullname"
-                                            className={styles.inputField}
-                                            // onChange={(e) => setName(e.target.value)}
-                                            placeholder="Số điện thoại"
-                                            required
-                                        />
-                                    </div>
-                                    <button
-                                        type="submit"
-                                        // onClick={handleLogin}
-                                        className={`${styles.buttonSubmit} ${styles.active}`}
-                                    >
-                                        Tra cứu
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        {info ? <Infomation/> :<Infomation/> }
-                      
-                    </div>
+  function handleSubmit() {}
 
+  function validateEmail() {
+    if (!mail.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+      return false;
+    }
+    return true;
+  }
 
-                </div>
-
+  return (
+    <div className={` ${styles.containALl}`}>
+      <div className="container">
+        <div
+          className={`row ${styles.wrap} justify-content-center`}
+          style={{ marginTop: "10rem" }}
+        >
+          <div className="col-lg-6">
+            <div style={{ textAlign: "center" }} className={styles.Recruit}>
+              <div className={styles.formRecruit}>
+                <form encType="multipart/form-data">
+                  <h2 className={styles.title}>Tra cứu kết quả ứng tuyển</h2>
+                  <p className={styles.text}>
+                    Anh / Chị vui lòng nhập cả 2 thông tin sau đây
+                  </p>
+                  <div className={styles.inputName}>
+                    <input
+                      type="number"
+                      className={styles.inputField}
+                      onChange={(e) => setPhone(e.target.value)}
+                      placeholder="Số điện thoại"
+                      pattern="/^[0-9]$/"
+                      required
+                    />
+                    {console.log(phone)}
+                  </div>
+                  <div className={styles.inputName}>
+                    <input
+                      type="email"
+                      pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                      className={styles.inputField}
+                      onChange={(e) => setMail(e.target.value)}
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      handleSubmit();
+                    }}
+                    className={`${styles.buttonSubmit}`}
+                    style={
+                      phone.length === 10 && validateEmail()
+                        ? {
+                            backgroundColor: "#bf202e",
+                            color: "#fff",
+                            cursor: "pointer",
+                          }
+                        : {}
+                    }
+                  >
+                    Tra cứu
+                  </button>
+                </form>
+              </div>
             </div>
+          </div>
+          <div className="col-lg-6">
+            {info ? <Infomation /> : <Infomation />}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default FormResearch
+export default FormResearch;
